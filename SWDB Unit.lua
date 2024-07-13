9cd0891b27836d2476a5057e6a98d6778aa63fbb
SlopeSlayer910 <SlopeSlayer910@gmail.com>
2024-07-13 16:28:52 +0200

    Removed the old input code comment block at the top. Added handeling for different types of units. Added Pass through and inactive bus states. Renamed keys to key. added some basic input handeling according to the end goal

--- SWDB Unit.lua
+++ SWDB Unit.lua
@@ -44,43 +44,14 @@ end
 
 -- try require("Folder.Filename") to include code from another file in this, so you can store code in libraries
 -- the "LifeBoatAPI" is included by default in /_build/libs/ - you can use require("LifeBoatAPI") to get this, and use all the LifeBoatAPI.<functions>!
---[[
-function uint32ToBoolTable(uint32Value)
-    local boolTable = {}
-    local mask = 1
-
-    for i = 32, 1, -1 do
-        boolTable[i] = (uint32Value & mask) ~= 0
-        mask = mask << 1
-    end
-
-    return boolTable
+unit = {}
+unit.type = 0
+if unit.type == 0 then
+    unit.address = 0
+else
+    unit.address = -1
 end
-
-function onTick()
-	float = input.getNumber(2)
-	packedData = string.pack("f", float)
-	int = string.unpack("I4", packedData)
-	output.setNumber(1, int)
-	output.setNumber(2, float)
-	boolTable = uint32ToBoolTable(int)
-	for i = 1, 32, 1 do
-		output.setBool(i, boolTable[i])
-	end
-	returnFlag = (int >> 31 & 1)
-	activeFlag = {int >> 30 & 1}
-	instructions = (int >> 23 & (2^7-1))
-	senderAddr = (int >> 16 & (2^7-1))
-	reciverAddr = (int >> 9 & (2^7-1))
-	data = (int & (2^9-1))
-	output.setNumber(3, instructions)
-	output.setNumber(4, senderAddr)
-	output.setNumber(5, reciverAddr)
-	output.setNumber(6, data)
-end
---]]
-
-keys = {"returnFlag", "active", "inst", "sender", "reciever","data"}
+key = {"returnFlag", "busFreeFlag", "instruction", "senderAddr", "recieverAddr","data"}
 incoming = {}
 outgoing = {}
 function onTick() --input
@@ -88,23 +59,56 @@ function onTick() --input
     incoming.packedData = string.pack("f", incoming.floatValue)
 	incoming.int = string.unpack("I4", incoming.packedData)
     --incoming data
-    incoming[keys[1]] = (incoming.int >> 31 & 1)
-	incoming[keys[2]] = (incoming.int >> 30 & 1)
-	incoming[keys[3]] = (incoming.int >> 23 & (2^7-1))
-	incoming[keys[4]] = (incoming.int >> 16 & (2^7-1))
-	incoming[keys[5]] = (incoming.int >> 9 & (2^7-1))
-	incoming[keys[6]] = (incoming.int & (2^9-1))
+    incoming[key[1]] = (incoming.int >> 31 & 1)
+	incoming[key[2]] = (incoming.int >> 30 & 1)
+	incoming[key[3]] = (incoming.int >> 23 & (2^7-1))
+	incoming[key[4]] = (incoming.int >> 16 & (2^7-1))
+	incoming[key[5]] = (incoming.int >> 9 & (2^7-1))
+	incoming[key[6]] = (incoming.int & (2^9-1))
 
-    --outgoing data
-    outgoing[keys[1]] = 1
-    outgoing[keys[2]] = 1
-    outgoing[keys[3]] = 126
-    outgoing[keys[4]] = 127
-    outgoing[keys[5]] = 63
-    outgoing[keys[6]] = incoming[keys[6]] + 1
+    --default bus to setBusPassthrough
+    setBusPassthrough()
+
+    --handle incoming data
+    if unit.type == 0 then
+        --if unit is a Master
+        if incoming[key[3]] == 0 then --idReq/idProv
+            if incoming[key[1]] == 0 then --idReq
+                --TODO Add handeling for incoming idReq to a master unit 
+                --HACK Using setBusInactive() as intrum fix for above
+                setBusInactive()
+            elseif incoming[key[1]] == 1 then --idProv
+                --if the idProv hasn't been pulled off the bus before being handed back to the master, pull it off the bus.
+                setBusInactive()
+            end
+        else
+            setBusPassthrough()
+        end
+    elseif type == 1 then
+        --if unit is a normal unit
+        if incoming[key[3]] == 0 then --idReq/idProv
+            if incoming[key[1]] == 1 then --idReq
+                --pass on the idReq
+                setBusPassthrough()
+            elseif incoming[key[1]] == 1 then --idProv
+                --check the incoming idProv to see if it is able to be used by this unit, if it is take it off the bus and assign this unit the provided number. if not then pass it on.
+                if (incoming[key[6]] >> 7) == unit.type then --if the two greatest data bits which indicate the type match the unit type then take it off the bus and assign this unit the provided number. if not then pass it on.
+                    setBusInactive()
+                    unit.address = incoming[key[6]] & (2^7-1) --set the unit address to the address provided by the idProv
+                else
+                    setBusPassthrough()
+                end
+            end
+        else
+            setBusPassthrough()
+        end
+    end
+    
+    --add own instructions if the outgoing bus is Inactive
+    --TODO add way to add own instructions if the outgoing bus is Inactive
 
     --outbound packet
-    outgoing.int = (outgoing[keys[1]] << 31 | outgoing[keys[2]] << 30 | outgoing[keys[3]] << 23 | outgoing[keys[4]] << 16 | outgoing[keys[5]] << 9 | outgoing[keys[6]])
+    outgoing.int = (outgoing[key[1]] << 31 | outgoing[key[2]] << 30 | outgoing[key[3]] << 23 | outgoing[key[4]] << 16 | outgoing[key[5]] << 9 | outgoing[key[6]])
     output.setNumber(1, outgoing.int)
     outgoing.packedData = string.pack("I4", outgoing.int)
     outgoing.floatValue = string.unpack("f", outgoing.packedData)
@@ -116,15 +120,33 @@ function onDraw()
 	screen.drawClear()
 	screen.setColor(0, 255, 0)
     local i = 0
-    for key, value in pairs(keys) do
+    for key, value in pairs(key) do
         i = i + 1
         screen.drawText(2, 6*i-4, string.sub(value, 1, 3) .. " = " .. incoming[value])
     end
 
     screen.setColor(255, 0, 0)
 
-    for key, value in pairs(keys) do
+    for key, value in pairs(key) do
         i = i + 1
         screen.drawText(2, 6*i-2, string.sub(value, 1, 3) .. " = " .. outgoing[value])
     end
+end
+
+function setBusInactive()
+    outgoing[key[1]] = 0
+    outgoing[key[2]] = 1
+    outgoing[key[3]] = 0
+    outgoing[key[4]] = 0
+    outgoing[key[5]] = 0
+    outgoing[key[6]] = 0
+end
+
+function setBusPassthrough()
+    outgoing[key[1]] = incoming[key[1]]
+    outgoing[key[2]] = incoming[key[2]]
+    outgoing[key[3]] = incoming[key[3]]
+    outgoing[key[4]] = incoming[key[4]]
+    outgoing[key[5]] = incoming[key[5]]
+    outgoing[key[6]] = incoming[key[6]]
 end
