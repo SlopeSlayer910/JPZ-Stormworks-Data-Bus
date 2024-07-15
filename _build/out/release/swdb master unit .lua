d92f15d908092fda47e5213ef483e399c656fe16
SlopeSlayer910 <SlopeSlayer910@gmail.com>
2024-07-15 11:07:32 +0200

    Latest generated outs

--- _build/out/release/swdb master unit .lua
+++ _build/out/release/swdb master unit .lua
@@ -5,29 +5,29 @@
 -- Developed & Minimized using LifeBoatAPI - Stormworks Lua plugin for VSCode
 -- https://code.visualstudio.com/download (search "Stormworks Lua with LifeboatAPI" extension)
 --      By Nameous Changey
--- Minimized Size: 1564 (1960 with comment) chars
+-- Minimized Size: 1606 (2002 with comment) chars
 D="I4"
 C=" = "
 B="f"
 
-l=pairs
+m=pairs
 i=false
-p=true
+u=true
 r=screen
-q=string
-y=q.sub
-u=r.drawText
-n=r.setColor
+p=string
+y=p.sub
+n=r.drawText
+l=r.setColor
 k=output.setNumber
-w=q.unpack
-z=q.pack
-_={"returnFlag","busFreeFlag","instruction","senderAddr","recieverAddr","data"}b={}a={}x=1
-c={}c.type=0
-c.t=0
-c.j=0
-g={}g[0]={type=0,h=p}for d=1,125,1 do
-g[d]={type=1,h=i}end
-g[63]={type=2,h=i}g[126]={type=2,h=i}g[127]={type=3,h=p}function onTick()b.s=input.getNumber(x)b.m=z(B,b.s)b.f=w(D,b.m)b[_[1]]=(b.f>>31 & 1)b[_[2]]=(b.f>>30 & 1)b[_[3]]=(b.f>>23 &(2^7-1))b[_[4]]=(b.f>>16 &(2^7-1))b[_[5]]=(b.f>>9 &(2^7-1))b[_[6]]=(b.f &(2^9-1))v()if b[_[2]]==0 then
+w=p.unpack
+z=p.pack
+_={"returnFlag","busFreeFlag","instruction","senderAddr","recieverAddr","data"}b={}a={}v=1
+d={}d.type=0
+d.t=0
+d.j=0
+h={}h[0]={type=0,g=u}for c=1,125,1 do
+h[c]={type=1,g=i}end
+h[63]={type=2,g=i}h[126]={type=2,g=i}h[127]={type=3,g=u}function onTick()b.q=input.getNumber(v)b.s=z(B,b.q)b.e=w(D,b.s)b[_[1]]=(b.e>>31 & 1)b[_[2]]=(b.e>>30 & 1)b[_[3]]=(b.e>>23 &(2^7-1))b[_[4]]=(b.e>>16 &(2^7-1))b[_[5]]=(b.e>>9 &(2^7-1))b[_[6]]=(b.e &(2^9-1))x()if b[_[2]]==0 then
 if b[_[3]]==0 then
 if b[_[1]]==0 then
 a[_[1]]=1
@@ -36,27 +36,27 @@ a[_[3]]=0
 a[_[4]]=0
 a[_[5]]=127
 a[_[6]]=b[_[6]]<<7
-for A,e in l(g)do
-if e.h==i and e.type==b[_[6]]then
+for A,f in m(h)do
+if f.g==i and f.type==b[_[6]]then
 a[_[6]]=a[_[6]]|A
-e.h=p
+f.g=u
 break
 end
 end
 elseif b[_[1]]==1 then
-local t=b[_[6]]&(2^7-1)g[t].h=i
+local t=b[_[6]]&(2^7-1)h[t].g=i
 o()end
 elseif b[_[3]]==1 then
-for d=1,126 do
-g[d].h=i
+for c=1,126 do
+h[c].g=i
 end
 o()else
-v()end
+x()end
 else
 o()end
 if a[_[2]]==1 then
-if c.j>60 then
-c.j=0
+if i and d.j>60 then
+d.j=0
 a[_[1]]=0
 a[_[2]]=0
 a[_[3]]=1
@@ -65,16 +65,17 @@ a[_[5]]=127
 a[_[6]]=0
 end
 end
-a.f=(a[_[1]]<<31|a[_[2]]<<30|a[_[3]]<<23|a[_[4]]<<16|a[_[5]]<<9|a[_[6]])k(1,a.f)a.m=z(D,a.f)a.s=w(B,a.m)k(x,a.s)c.j=c.j+1
-k(2,c.type)k(3,c.t)k(4,c.j)end
-function onDraw()n(20,20,20)r.drawClear()n(0,255,0)local d=0
-for _,e in l(_)do
-d=d+1
-u(2,6*d-4,y(e,1,3)..C..b[e])end
-n(255,0,0)for _,e in l(_)do
-d=d+1
-u(2,6*d-2,y(e,1,3)..C..a[e])end
-end
+a.e=(a[_[1]]<<31|a[_[2]]<<30|a[_[3]]<<23|a[_[4]]<<16|a[_[5]]<<9|a[_[6]])k(1,a.e)a.s=z(D,a.e)a.q=w(B,a.s)k(v,a.q)d.j=d.j+1
+k(2,d.type)k(3,d.t)k(4,d.j)end
+function onDraw()l(20,20,20)r.drawClear()l(0,255,0)local c=0
+for _,f in m(_)do
+c=c+1
+n(2,6*c-4,y(f,1,3)..C..b[f])end
+l(255,0,0)for _,f in m(_)do
+c=c+1
+n(2,6*c-2,y(f,1,3)..C..a[f])end
+l(0,0,0)c=c+1
+n(2,6*c,"Master Unit")end
 function o()a[_[1]]=0
 a[_[2]]=1
 a[_[3]]=0
@@ -82,4 +83,4 @@ a[_[4]]=0
 a[_[5]]=0
 a[_[6]]=0
 end
-function v()a[_[1]]=b[_[1]]a[_[2]]=b[_[2]]a[_[3]]=b[_[3]]a[_[4]]=b[_[4]]a[_[5]]=b[_[5]]a[_[6]]=b[_[6]]end
+function x()a[_[1]]=b[_[1]]a[_[2]]=b[_[2]]a[_[3]]=b[_[3]]a[_[4]]=b[_[4]]a[_[5]]=b[_[5]]a[_[6]]=b[_[6]]end
--- _build/out/release/swdb unit.lua
+++ _build/out/release/swdb unit.lua
@@ -5,39 +5,39 @@
 -- Developed & Minimized using LifeBoatAPI - Stormworks Lua plugin for VSCode
 -- https://code.visualstudio.com/download (search "Stormworks Lua with LifeboatAPI" extension)
 --      By Nameous Changey
--- Minimized Size: 1271 (1667 with comment) chars
+-- Minimized Size: 1300 (1696 with comment) chars
 x="I4"
 w=" = "
 v="f"
 
-u=pairs
-n=screen
-m=string
-o=m.sub
-p=n.drawText
-k=n.setColor
+s=pairs
+k=screen
+n=string
+p=n.sub
+o=k.drawText
+j=k.setColor
 i=output.setNumber
-t=m.unpack
-q=m.pack
-_={"returnFlag","busFreeFlag","instruction","senderAddr","recieverAddr","data"}b={}a={}s=1
+t=n.unpack
+r=n.pack
+_={"returnFlag","busFreeFlag","instruction","senderAddr","recieverAddr","data"}b={}a={}u=1
 c={}c.type=1
-c.g=-1
-function onTick()b.l=input.getNumber(s)b.j=q(v,b.l)b.d=t(x,b.j)b[_[1]]=(b.d>>31 & 1)b[_[2]]=(b.d>>30 & 1)b[_[3]]=(b.d>>23 &(2^7-1))b[_[4]]=(b.d>>16 &(2^7-1))b[_[5]]=(b.d>>9 &(2^7-1))b[_[6]]=(b.d &(2^9-1))h()if b[_[2]]==0 then
+c.f=-1
+function onTick()b.m=input.getNumber(u)b.l=r(v,b.m)b.d=t(x,b.l)b[_[1]]=(b.d>>31 & 1)b[_[2]]=(b.d>>30 & 1)b[_[3]]=(b.d>>23 &(2^7-1))b[_[4]]=(b.d>>16 &(2^7-1))b[_[5]]=(b.d>>9 &(2^7-1))b[_[6]]=(b.d &(2^9-1))h()if b[_[2]]==0 then
 if b[_[3]]==0 then
 if b[_[1]]==0 then
 h()elseif b[_[1]]==1 then
-if(b[_[6]]>>7)==c.type and c.g==-1 then
-c.g=b[_[6]]&(2^7-1)r()else
+if(b[_[6]]>>7)==c.type and c.f==-1 then
+c.f=b[_[6]]&(2^7-1)q()else
 h()end
 end
-elseif b[_[3]]==0 then
-c.g=-1
+elseif b[_[3]]==1 then
+c.f=-1
 h()else
 h()end
 else
-r()end
+q()end
 if a[_[2]]==1 then
-if c.g==-1 then
+if c.f==-1 then
 a[_[1]]=0
 a[_[2]]=0
 a[_[3]]=0
@@ -46,16 +46,17 @@ a[_[5]]=0
 a[_[6]]=c.type
 end
 end
-a.d=(a[_[1]]<<31|a[_[2]]<<30|a[_[3]]<<23|a[_[4]]<<16|a[_[5]]<<9|a[_[6]])i(1,a.d)a.j=q(x,a.d)a.l=t(v,a.j)i(s,a.l)i(2,c.type)i(3,c.g)end
-function onDraw()k(20,20,20)n.drawClear()k(0,255,0)local e=0
-for _,f in u(_)do
+a.d=(a[_[1]]<<31|a[_[2]]<<30|a[_[3]]<<23|a[_[4]]<<16|a[_[5]]<<9|a[_[6]])i(1,a.d)a.l=r(x,a.d)a.m=t(v,a.l)i(u,a.m)i(2,c.type)i(3,c.f)end
+function onDraw()j(20,20,20)k.drawClear()j(0,255,0)local e=0
+for _,g in s(_)do
 e=e+1
-p(2,6*e-4,o(f,1,3)..w..b[f])end
-k(255,0,0)for _,f in u(_)do
+o(2,6*e-4,p(g,1,3)..w..b[g])end
+j(255,0,0)for _,g in s(_)do
 e=e+1
-p(2,6*e-2,o(f,1,3)..w..a[f])end
-end
-function r()a[_[1]]=0
+o(2,6*e-2,p(g,1,3)..w..a[g])end
+j(0,0,0)e=e+1
+o(2,6*e,"Unit")end
+function q()a[_[1]]=0
 a[_[2]]=1
 a[_[3]]=0
 a[_[4]]=0
